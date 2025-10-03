400ac9752cd7dffb12f9fde5bf75eb13bbee8e16
ClenildonFerreira <49656737+ClenildonFerreira@users.noreply.github.com>
2025-10-03 14:47:03 -0300

    feat: criando projeto

---
+++ src/main/java/com/clenildonferreira/delrio_todolist_api/DelrioTodolistApiApplication.java
@@ -0,0 +1,13 @@
+package com.clenildonferreira.delrio_todolist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelrioTodolistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelrioTodolistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clenildonferreira/delrio_todolist_api/DelrioTodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clenildonferreira.delrio_todolist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelrioTodolistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
